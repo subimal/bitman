6ec1ef0bed9acb641139a37f20a4686bcb83b138
Subimal Deb <subimal.deb@gmail.com>
2020-12-15 15:11:08 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# bitman
+
+## What is bitman?
+This is a pure python module for MANaging an array of BITs. 
+
+## How does it work?
+bitman takes advantage of python's ability to handle big integers. The array of bits is simply an integer with its bit values set as desired. 
+
+The array of bits is a class called bitman.
+
+## Attributes of bitman
+
+* **size**: It is an integer that holds the array size.
+* **set(n, val)**: A function that sets the n-th bit to val.
+* **get(n)**:	A function that returns the truth value of the n-th bit.
+* **which(val)**: A list of positions with a value val (0 or 1).
+
+
+## Examples
+
+### Initializing a bitman instance
+
+```python
+from bitman import bitman
+a = bitman(size=5,init=1)
+```
+
+
---
+++ bitman.py
@@ -0,0 +1,49 @@
+class bitman:
+    def __init__(self, size=0, init=0):
+        '''
+        Initializes the bitman class with 'size' bits initialized to init (either 0 or 1)
+        '''
+        
+        assert init in [0,1], "Initialize the bits to 0 or 1"
+        self.size = size
+        if (init==0):
+            self.bits=0
+        elif (init==1):
+            self.bits = 2**(size+1)-1
+
+    def __repr__(self):
+        #return bin(self.bits)[2:-1]
+        ans = {True:'1', False:'0'}
+        return ''.join([ ans[((self.bits & (1<<i))>0)] for i in range(self.size-1,-1,-1)])
+
+    def get(self, n):
+        '''
+        Get the n-th bit.
+        '''
+        return (self.bits & (1<<n))>0
+
+    def set(self, n, val):
+        '''Set the n-th bit to val. 
+        val is either 1 or 0.'''
+        
+        if (val == 1):
+            self.bits = self.bits | 1<<n
+        else:
+            tmp = 2**(max( [self.size, n])) -1
+            tmp = tmp - (1<<n)
+            self.bits = (self.bits & tmp)
+            if n>self.size:
+                self.size=n
+            
+
+    def which(self, val):
+        '''
+        Returns a list of positions of bits with a value val (that is 0 or 1).
+        '''
+        ans = []
+        for i in range(self.size):
+                if self.get(i)==1:
+                    ans.append(i)
+        return ans
+
+

c5c0caf75b84c9765c56bce00bbf60d0899bfcd8
Subimal Deb <subimal.deb@gmail.com>
2020-12-19 00:11:11 +0530

    Added __init__.py and modified setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bitman-subimal", # Replace with your own username
-    version="0.0.1",
+    name="bitman", # Replace with your own username
+    version="1.0.1",
     author="Subimal Deb",
     author_email="subimal.deb@gmail.com",
     description="A purepython bit manager",
@@ -15,8 +15,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GPLv3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=2.7',
 )
+
+
